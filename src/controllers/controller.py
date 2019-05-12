982f63ab63400949145ab6ec6da87e4d0a967d40
foxale <lispawel24@gmail.com>
2019-05-12 16:36:55 +0200

    Simplify preparing beverage in view
    Since the only beverage available for now is the coffee, there is no
    need for the prepare_beverage wrapper.

--- src/controllers/controller.py
+++ src/controllers/controller.py
@@ -39,20 +39,16 @@ class Controller:
         choice: str = self.view.prompt_user_with_actions_on_coffee_machine(coffee_machine_is_on=self.coffee_machine.is_on)
         if 'turn the coffee machine' in choice:
             self.coffee_machine.turn_on() if 'ON' in choice else self.coffee_machine.turn_off()
-        elif 'drink' in choice:
-            _, serving, beverage, *_ = choice.split(' ')
+        elif 'coffee' in choice:
+            _, serving, *_ = choice.split(' ')  # as in 'drink large coffee with milk'
             with_milk = 'with milk' in choice
-            self.prepare_beverage(beverage=beverage, serving=serving, with_milk=with_milk)
+            self.prepare_coffee(serving=serving, with_milk=with_milk)
         elif choice == 'go away':
             self.play = False
         elif choice == 'refill water':
             self.coffee_machine.refill_water()
         elif choice == 'refill milk':
             self.coffee_machine.refill_milk()
-
-    def prepare_beverage(self, beverage: str, serving: str, with_milk: bool) -> None:
-        if beverage == 'coffee':
-            self.prepare_coffee(serving=serving, with_milk=with_milk)
 
     def prepare_coffee(self, serving: str, with_milk: bool) -> None:
         try:
